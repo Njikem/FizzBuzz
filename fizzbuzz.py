a8f8ad12630b9d5eaf9f2275b420f4acadd5fb45
Njikem <123434988+Njikem@users.noreply.github.com>
2023-05-31 00:22:03 -0600

    fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,46 @@
+def fizzbuzz(x):
+    
+    if(x % 3 == 0 and x % 5 == 0):
+         return ("fizzbuzz")
+    elif(x % 3 == 0):
+        return("fizz")
+    elif(x % 5 == 0):
+        return("buzz")
+    else:
+        return("No fizzbuzz")
+    
+my_fizz = fizzbuzz(15)
+print(my_fizz)
+my_buzz = fizzbuzz(30)
+print(my_buzz)
+my_numb = fizzbuzz(90)
+print(my_numb)
+my_num = fizzbuzz(10)
+print(my_num)
+my_fiz = fizzbuzz(25)
+print(my_fiz)
+my_buz = fizzbuzz(40)
+print(my_buz)
+my_fizs = fizzbuzz(45)
+print(my_fizs)
+my_numbs = fizzbuzz(19)
+print(my_numbs)
+my_app = fizzbuzz(35)
+print(my_app)
+my_apps = fizzbuzz(22)
+print(my_apps)
+my_number = fizzbuzz(42)
+print(my_number)
+my_numbers = fizzbuzz(74)
+print(my_numbers)
+my_no_fizz = fizzbuzz(9)
+print(my_no_fizz)
+my_buzzs = fizzbuzz(12)
+print(my_buzzs)
+my_buz = fizzbuzz(31)
+print(my_buz)
+    
+    
+    
+    
+            
